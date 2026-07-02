3260cfc5399bfd0c9ec75dffcf3b8d523f9ed0b4
AYUSH <dixitayush6394@gmail.com>
2026-07-02 19:00:45 +0530

    fix brevo env

--- backend/src/config/brevo.js
+++ backend/src/config/brevo.js
@@ -1,14 +1,11 @@
 import * as Brevo from "@getbrevo/brevo";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const client = Brevo.ApiClient.instance;
-
-// API key
-client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
-
-// Transactional email API
 const transactionalEmailApi = new Brevo.TransactionalEmailsApi();
+
+
+transactionalEmailApi.setApiKey(
+  Brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
 
 export default transactionalEmailApi;
